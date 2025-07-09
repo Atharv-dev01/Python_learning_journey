d4fd35091863bd4b098c04fa8b15a14b567f6739
Atharv patil <atharvpatil036@gmail.com>
2025-07-09 22:57:49 +0530

    Add Day 1 Practice code:Variables and Data types

---
+++ Day 1- Variables and Data types/Practice_code.py
@@ -0,0 +1,117 @@
+#using print:
+print("Hello world !")
+print("Atharv is my name.")
+print("My age is 20.")
+print("Atharv is my name.","My age is 20.")
+print(23)
+print(35+23)
+
+#variables:
+name = "Atharv"
+age=20
+price =25.8
+print(name)
+print(age)
+print(price)
+print("my name is : ", name)
+print("my age is : ", age)
+print("price is : ", price)
+age2=age
+print(age2)
+namex =name
+print (namex)
+print(type(name))
+print(type(age))
+print(type(price))
+
+name1='patil'
+name2="atharv"
+name3="""dattatray"""
+print(name1)
+print(name2)
+print(name3)
+a=2
+b=98.5
+sum=100.5
+print(sum)
+
+#Data types
+print(type(a))
+print(type(b))
+print(type(sum))
+
+a=2
+b=10
+sum=a+b
+print(sum)
+
+multiply=a*b
+print(multiply)
+
+divide=a/b
+print(divide)
+
+substract1=b-a
+print(substract1)
+
+substract2=a-b
+print(substract2)
+
+print(type(multiply))
+print(type(divide))
+print(type(substract1))
+print(type(substract2))
+ 
+ #single line comment:
+print("hello world")
+#print("hello world")
+
+#multi line comment:
+print("hello world")
+
+"""
+multi-line
+comment
+
+"""
+#arithmetic operators
+a=5
+b=5
+
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+print(a%b)#remainder
+print(a**b)
+
+#relational operators
+a=5
+b=10
+
+print(a==b)
+print(a!=b)
+print(a>b)
+print(a<b)
+print(a>=b)
+print(a<=b)
+
+#assignment operator
+num=10
+num+=10
+print("num:",num )
+
+num-=10
+print("num:",num )
+
+num*=10
+print("num:",num )
+
+num/=10
+print("num:",num )
+
+num%=10
+print("num:",num )
+
+num**=10
+print("num:",num )
